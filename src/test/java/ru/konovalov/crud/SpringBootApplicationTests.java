80296f0787ce85184bceee1757186cd026cfa4f9
Konovalov_Oleg <oledjan2014@gmail.com>
2022-08-01 15:19:40 +0300

    commit

--- src/test/java/ru/konovalov/crud/SpringBootApplicationTests.java
+++ src/test/java/ru/konovalov/crud/SpringBootApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Pp311SpringBootApplicationTests {
+class SpringBootApplicationTests {
 
 	@Test
 	void contextLoads() {
